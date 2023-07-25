2b45a4fc8ef3ec64f94b2692d2a8399d0738ed79
ludovic EIGENMANN <a.ludovic.eigenmann@net.dev>
2023-07-25 09:53:12 +0200

    Initial Commit

---
+++ src/test/java/ludev/fullstack/depo/AUTH_JWT/AuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ludev.fullstack.depo.AUTH_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
